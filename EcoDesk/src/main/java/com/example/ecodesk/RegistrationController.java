8dda83c575bcf3d370f31e1b45e13f3dba2ae167
Miguel Zinedinne Guzmán Sainz <miguelzinedinne@gmail.com>
2024-01-10 04:31:38 -0600

    sign in created

--- EcoDesk/src/main/java/com/example/ecodesk/RegistrationController.java
+++ EcoDesk/src/main/java/com/example/ecodesk/RegistrationController.java
@@ -1,15 +1,37 @@
 package com.example.ecodesk;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 public class RegistrationController {
 
     @FXML
     private TextField nombreField;
+
+    @FXML
+    private Label welcomeText;
 
     @FXML
     private TextField apellidoPaternoField;
@@ -30,7 +52,7 @@ public class RegistrationController {
     private PasswordField repetirPasswordField;
 
     @FXML
-    private ImageView myImageView;  // Debes tener un ImageView con este ID en tu FXML.
+    private ImageView myImageView;
 
     @FXML
     public void initialize() {
@@ -44,6 +66,19 @@ public class RegistrationController {
         myImageView.setFitWidth(200);  // Ancho deseado
         myImageView.setFitHeight(200); // Alto deseado
     }
+
+    @FXML
+    protected void returnToLogin() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+
+        HelloController helloController = loader.getController();
+
+        Stage currentStage = (Stage) myImageView.getScene().getWindow();
+        currentStage.setScene(new Scene(root));
+        currentStage.setTitle("EcoDesk - Login");
+        currentStage.show();
+    }
     @FXML
     protected void registerUser() {
         String nombre = nombreField.getText();
@@ -53,5 +88,64 @@ public class RegistrationController {
         String telefono = telefonoField.getText();
         String password = passwordField.getText();
         String repetirPassword = repetirPasswordField.getText();
+
+        if (nombre.isEmpty() || apellidoPaterno.isEmpty() || apellidoMaterno.isEmpty() ||
+                correo.isEmpty() || telefono.isEmpty() || password.isEmpty() || repetirPassword.isEmpty()) {
+            welcomeText.setText("Todos los campos son requeridos");
+            return;
+        } else if ((!password.equals(repetirPassword))) {
+            welcomeText.setText("Las contraseñas no coinciden");
+            return;
+        } else {
+            String formattedTelefono = "tel:+52-" + telefono.substring(0, 3) + "-" + telefono.substring(3, 6) + "-" + telefono.substring(6);
+            String jsonInputString = "{"
+                    + "\"Nombre\": \"" + nombre + "\","
+                    + "\"ApellidoPaterno\": \"" + apellidoPaterno + "\","
+                    + "\"ApellidoMaterno\": \"" + apellidoMaterno + "\","
+                    + "\"Email\": \"" + correo + "\","
+                    + "\"UrlImagen\": \"\","
+                    + "\"PerfilPublico\": true,"
+                    + "\"Guia\": false,"
+                    + "\"Telefono\": \"" + formattedTelefono + "\","
+                    + "\"Bitacoras\": [],"
+                    + "\"Clave\": \"" + password + "\""
+                    + "}";
+            try {
+                String url = "http://ec2-3-137-140-200.us-east-2.compute.amazonaws.com:8000/usuarios/crear";
+
+                HttpClient httpClient = HttpClients.createDefault();
+                HttpPost httpPost = new HttpPost(url);
+
+                StringEntity entity = new StringEntity(jsonInputString);
+                httpPost.setEntity(entity);
+                httpPost.setHeader("Accept", "application/json");
+                httpPost.setHeader("Content-type", "application/json");
+
+                HttpResponse response = httpClient.execute(httpPost);
+
+                String responseBody = EntityUtils.toString(response.getEntity());
+
+                if (response.getStatusLine().getStatusCode() == 201) {
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Éxito");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Usuario creado exitosamente");
+                    alert.showAndWait();
+                    returnToLogin();
+                } else {
+                    // Manejar otros códigos de respuesta si es necesario
+                    System.out.println("Error al crear usuario: " + response.getStatusLine().getStatusCode());
+                }
+
+                System.out.println(responseBody);
+                welcomeText.setText("creamos el json "+ jsonInputString);
+                System.out.println(jsonInputString);
+                return;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
